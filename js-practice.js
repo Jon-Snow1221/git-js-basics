69e0e7e5425e23e43f2bb58541beba8673198151
Jonathan Llewellyn <jonathan.llewellyn27@gmail.com>
2021-09-07 13:12:04 -0600

    Initial commit

---
+++ js-practice.js
@@ -0,0 +1,30 @@
+// console.log("Hello World")
+
+const firstName = "Jonathan";
+let age = 30;
+
+console.log(`Hello, my name is ${firstName}. I'm ${age} years old.`)
+
+let myBool = false
+
+if (myBool === true){
+    console.log('hey hey')
+
+}
+let myFavNum = 30;
+
+if(myFavNum >= 30) {
+    console.log('Your number is awesome!')
+}
+
+const num1 = 30;
+let num2 = 25;
+
+num2 = 99;
+console.log(num1 > num2)
+
+const multiplicationOfNums = num1 * num2
+const divisionOfNums = num1 / num2
+
+console.log(multiplicationOfNums, divisionOfNums)
+
